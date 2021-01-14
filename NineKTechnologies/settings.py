f612408be045abea02f4b617e2ac18477775d8e0
Prashan Pudasaini <ppprojects20@gmail.com>
2021-01-14 15:05:59 -0600

    Update settings.py

--- NineKTechnologies/settings.py
+++ NineKTechnologies/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'spqto2x8q&ktiups4yn16j+94l(nj-r(&t+vg9x%0=wil9q(3b'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nktdb',
         'USER': 'postgres',
-        'PASSWORD': 'Prashanpassw0rd',
+        'PASSWORD': 'YOUR_PASSWORD',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -101,7 +101,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-FINANCIAL_MODELING_PREP_API_KEY = '4ea5593093f3202bae9ca992c7ff4ce6'
+API = 'YOUR_API'
 
 LOGIN_REDIRECT_URL = 'stock-most-popular'
 LOGIN_URL = 'login'
@@ -109,8 +109,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '9000technologies@gmail.com'
-EMAIL_HOST_PASSWORD = 'sqos nkww sbet foii'
+EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Rupy Team <noreply@rupy.com>'
+DEFAULT_FROM_EMAIL = 'DEFAULT_EMAIL'
